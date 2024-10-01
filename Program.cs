01453c634923323c41b80e18f89eb68105079b90
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-01 09:45:12 +0400

    modified the code hence it will show the options first then it ask user to enter the required values hence each shape can be calculated individually

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using static Simple_Hospital_Management_System2.Area;
-using System.Drawing;
 
 namespace Simple_Hospital_Management_System2
 {
@@ -8,61 +6,76 @@ namespace Simple_Hospital_Management_System2
     {
         static void Main(string[] args)
         {
-            // Get user input for length and width
-            Console.Write("Enter length: ");
-            double length = double.Parse(Console.ReadLine());
-            Console.Write("Enter width: ");
-            double width = double.Parse(Console.ReadLine());
+            // Prompt user for shape choice
+            Console.WriteLine("Please Select an shape:");
+            Console.WriteLine(" 1. Circle");
+            Console.WriteLine(" 2. Rectangle");
+            Console.WriteLine(" 3. Triangle");
+            Console.WriteLine(" 4. Square");
+            Console.WriteLine(" 5. Exit");
 
+            int Choice = int.Parse(Console.ReadLine());
 
-            // Create shapes and calculate areas
-            Shape C = new circle(width);
-            Shape R = new rectangle(length, width);
-            Shape T = new triangle(length, width);
-            Shape S = new square(width);
-            bool EnterFlag = false;
-
-            do
+            while (true)
             {
-                
-                try
+                switch (Choice)
                 {
-                    
-                    Console.WriteLine("Please Select an shape: ");
-                    Console.WriteLine(" 1. Circle. \n 2. Rectangle. \n 3. Triangle. \n 4. Square.\n5. Exit ");
-                    int Choice = int.Parse(Console.ReadLine());
-                    switch (Choice)
-                    {
-                        case 1:
-                            Console.Clear();
-                            C.Area();
-                            Console.WriteLine("Area of circle: " + C.Area());
-                            break;
-                        case 2:
-                            Console.Clear();
-                            R.Area();
-                            Console.WriteLine("Area of rectangle: " + R.Area());
-                            break;
-                        case 3:
-                            Console.Clear();
-                            T.Area();
-                            Console.WriteLine("Area of triangle: " + T.Area());
-                            break;
-                        case 4:
-                            Console.Clear();
-                            S.Area();
-                            Console.WriteLine("Area of square: " + S.Area());
-                            break;
-                        case 5:
-                            return;
-
-
-                    }
+                    case 1:
+                        // Get user input for radius
+                        Console.Write("Enter radius: ");
+                        double radius = double.Parse(Console.ReadLine());
+                        Shape C = new Circle(radius);
+                        Console.WriteLine("Area of circle: " + C.Area());
+                        break;
+                    case 2:
+                        // Get user input for length and width
+                        Console.Write("Enter length: ");
+                        double length = double.Parse(Console.ReadLine());
+                        Console.Write("Enter width: ");
+                        double width = double.Parse(Console.ReadLine());
+                        Shape R = new Rectangle(length, width);
+                        Console.WriteLine("Area of rectangle: " + R.Area());
+                        break;
+                    case 3:
+                        // Get user input for base and height
+                        Console.Write("Enter base: ");
+                        length = double.Parse(Console.ReadLine());
+                        Console.Write("Enter height: ");
+                        width = double.Parse(Console.ReadLine());
+                        Shape T = new Triangle(length, width);
+                        Console.WriteLine("Area of triangle: " + T.Area());
+                        break;
+                    case 4:
+                        // Get user input for side
+                        Console.Write("Enter side: ");
+                        length = double.Parse(Console.ReadLine());
+                        Shape S = new Square(length);
+                        Console.WriteLine("Area of square: " + S.Area());
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
                 }
-                catch (Exception ex) { Console.WriteLine("Invalid Input"); }
-            } while (EnterFlag != true);
-           
- 
+
+                // Prompt user for another shape choice
+                Console.WriteLine("Do you want to calculate the area of another shape? (y/n)");
+                string response = Console.ReadLine().ToLower();
+                if (response != "y")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Please Select an shape:");
+                Console.WriteLine(" 1. Circle");
+                Console.WriteLine(" 2. Rectangle");
+                Console.WriteLine(" 3. Triangle");
+                Console.WriteLine(" 4. Square");
+                Console.WriteLine(" 5. Exit");
+
+                Choice = int.Parse(Console.ReadLine());
+            }
         }
 
         public abstract class Shape
@@ -80,8 +93,10 @@ namespace Simple_Hospital_Management_System2
                 this.length = length;
                 this.width = width;
             }
+
             public abstract double Area();
         }
+
         class Square : Shape
         {
             public Square(double side) : base(side) { }
@@ -91,18 +106,10 @@ namespace Simple_Hospital_Management_System2
                 return length * length;
             }
         }
-        public class square : Shape
-        {
-            public square(double side) : base(side) { }
 
-            public override double Area()
-            {
-                return length * length;
-            }
-        }
-        class circle : Shape
+        class Circle : Shape
         {
-            public circle(double radius) : base(radius) { }
+            public Circle(double radius) : base(radius) { }
 
             public override double Area()
             {
@@ -110,9 +117,9 @@ namespace Simple_Hospital_Management_System2
             }
         }
 
-        class triangle : Shape
+        class Triangle : Shape
         {
-            public triangle(double baseLength, double height) : base(baseLength, height) { }
+            public Triangle(double baseLength, double height) : base(baseLength, height) { }
 
             public override double Area()
             {
@@ -120,15 +127,14 @@ namespace Simple_Hospital_Management_System2
             }
         }
 
-        class rectangle : Shape
+        class Rectangle : Shape
         {
-            public rectangle(double length, double width) : base(length, width) { }
+            public Rectangle(double length, double width) : base(length, width) { }
 
             public override double Area()
             {
                 return length * width;
             }
         }
-
     }
 }
